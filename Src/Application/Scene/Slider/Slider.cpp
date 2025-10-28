bf3642f72c55046199fb7bcb23065559b8ebc082
kd1367407 <kd1367407@st.kobedenshi.ac.jp>
2025-10-28 13:52:45 +0900

    一旦

---
+++ Src/Application/Scene/Slider/Slider.cpp
@@ -0,0 +1,51 @@
+﻿#include "Slider.h"
+#include"../../main.h"
+
+Slider::Slider(const Math::Vector2& pos, std::shared_ptr<KdTexture> backTex, std::shared_ptr<KdTexture> knobTex, std::function<void(float)> onValueChanged):
+	m_pos(pos),m_backTex(backTex),m_knobTex(knobTex),m_onValueChanged(onValueChanged)
+{
+	if (m_backTex)
+	{
+		m_size = { 
+			(float)m_backTex->GetInfo().Width,
+			(float)m_backTex->GetInfo().Height
+		};
+	}
+}
+
+void Slider::Update()
+{
+	float screenWidth = KdDirect3D::Instance().GetBackBuffer()->GetInfo().Width;
+	float screenHeight = KdDirect3D::Instance().GetBackBuffer()->GetInfo().Height;
+
+	m_rect = CalculateRectFromWorld(m_pos, m_size, screenWidth, screenHeight);
+
+	POINT mousePos;
+	GetCursorPos(&mousePos);
+	ScreenToClient(Application::Instance().GetWindowHandle(), &mousePos);
+
+	bool isOver = m_rect.Contains(static_cast<long>(mousePos.x), static_cast<long>(mousePos.y));
+
+	if (isOver && KdInputManager::Instance().IsHold("Select"))
+	{
+		//マウスのX座標をスライダーRectからの相対座標へ
+		float mouseRelX = static_cast<float>(mousePos.x - m_rect.x);
+		float newValue = mouseRelX / m_rect.width;
+
+		newValue = std::max(0.0f, std::min(1.0f, newValue));
+
+		if (m_value != newValue)
+		{
+			m_value = newValue;
+
+			if (m_onValueChanged)
+			{
+				m_onValueChanged(m_value);
+			}
+		}
+	}
+}
+
+void Slider::Draw(float baseAlpha)
+{
+}
--- Src/Application/Scene/Slider/Slider.h
+++ Src/Application/Scene/Slider/Slider.h
@@ -3,10 +3,20 @@
 class Slider
 {
 public:
-	Slider();
+	Slider(const Math::Vector2& pos,
+		std::shared_ptr<KdTexture> backTex,
+		std::shared_ptr<KdTexture> knobTex,
+		std::function<void(float)> onValueChanged);
+
+	void Update();
+	void Draw(float baseAlpha);
 
 private:
 	Math::Vector2 m_pos;
-	Math::Vector2 m_size;
-	Math::Rectangle m_angle;
+	Math::Vector2 m_size;//バーのサイズ
+	Math::Rectangle m_rect;//バーの判定
+	std::shared_ptr<KdTexture> m_backTex;
+	std::shared_ptr<KdTexture> m_knobTex;
+	float m_value;
+	std::function<void(float)> m_onValueChanged;
 };
